d4d76b115feb320269e385a91141fd032d5b7a42
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2012-09-03 22:05:41 -0300

    actualizacion documentacion del cliente.py

--- chileatiende/cliente.py
+++ chileatiende/cliente.py
@@ -21,18 +21,18 @@ import xml.dom.minidom
 class ChileAtiende:
 	""" Clase base que se encarga de establecer la conexion al sitio de ChileAtiende
 
-		Importante: Para poder hacer uso de los servicios REST provistos por la api
-					de ChileAtiende es necesario obtener el access_token, pues toda
-					comunicacion se realiza teniendo este token.
+	Importante: Para poder hacer uso de los servicios REST provistos por la api
+	de ChileAtiende es necesario obtener el access_token, pues toda
+	comunicacion se realiza teniendo este token.
 
-					Para poder utilizar la interfaz de programacion de chile atiende
-					se debe solicitar un token de acceso a traves de la siguiente URL:
+	Para poder utilizar la interfaz de programacion de chile atiende
+	se debe solicitar un token de acceso a traves de la siguiente URL:
 
-					https://www.chileatiende.cl/desarrolladores/access_token
+		https://www.chileatiende.cl/desarrolladores/access_token
 
-					Para mas antecedentes de la api se puede revisar la siguiente URL:
+	Para mas antecedentes de la api se puede revisar la siguiente URL:
 
-					https://www.chileatiende.cl/desarrolladores
+		https://www.chileatiende.cl/desarrolladores
 	"""
 	def __init__(self,access_token=None):
 		""" Constructor que inicializa la conexion al sitio de ChileAtiende """
@@ -62,25 +62,26 @@ class ChileAtiende:
 
 class Fichas(ChileAtiende):
 	""" La clase Fichas representa las Fichas de registro de los tramites disponibles
-		a traves de ChileAtiende. Deriva de la clase base ChileAtiende.
+	a traves de ChileAtiende. Deriva de la clase base ChileAtiende.
 
-		ejemplo de uso:
-			from chileatiende.clientes import Fichas
+	Ejemplo de uso:
+		
+		from chileatiende.clientes import Fichas
 
-			fichas = Fichas(access_token=u'PiKjqM1XAx1P3Wia')
+		fichas = Fichas(access_token=u'PiKjqM1XAx1P3Wia')
 	"""
 	def __init__(self,access_token=None):
 		ChileAtiende.__init__(self,access_token)
 
 	def obtener(self,fichaId=None,type='json',callback=''):
 		""" Metodo que permite obtener una ficha de acuerdo a su identificador unico
-			fichaId. El metodo consume el servicio REST:
+		fichaId. El metodo consume el servicio REST:
 
-				GET https://www.chileatiende.cl/api/fichas/{fichaId}
+			GET https://www.chileatiende.cl/api/fichas/{fichaId}
 
-			Nota: El valor por defecto de type es "json", esto indica que los datos
-			devueltos por el metodo estan en ese formato. Si type = "xml", los datos
-			son devueltos como un documento XML.
+		Nota: El valor por defecto de type es "json", esto indica que los datos
+		devueltos por el metodo estan en ese formato. Si type = "xml", los datos
+		son devueltos como un documento XML.
 		"""
 		self._connection.request('GET','/api/fichas/'+fichaId+'?access_token='+self._access_token+'&type='+type+'&callback='+callback)
 		
@@ -93,13 +94,13 @@ class Fichas(ChileAtiende):
 
 	def listar(self,type='json',callback='',query='',maxResults=10,pageToken=''):
 		""" Metodo que permite listar todas las fichas de los tramites registrados en ChileAtiende.
-			El metodo consume el servicio REST:
+		El metodo consume el servicio REST:
 			
-				GET https://www.chileatiende.cl/api/fichas
+			GET https://www.chileatiende.cl/api/fichas
 
-			Nota: El valor por defecto de type es "json", esto indica que los datos
-			devueltos por el metodo estan en ese formato. Si type = "xml", los datos
-			son devueltos como un documento XML.
+		Nota: El valor por defecto de type es "json", esto indica que los datos
+		devueltos por el metodo estan en ese formato. Si type = "xml", los datos
+		son devueltos como un documento XML.
 		"""
 		self._connection.request('GET','/api/fichas?access_token='+self._access_token+'&type='+type+'&callback='+callback+'&query='+query+'&maxResults='+str(maxResults)+'&pageToken='+pageToken)
 		
@@ -112,13 +113,13 @@ class Fichas(ChileAtiende):
 
 	def listarPorServicio(self,servicioId=None,type='json',callback=''):
 		""" Metodo que permite listar todas las fichas pertenencientes a un servicio publico.
-			El metodo consume el servicio REST:
+		El metodo consume el servicio REST:
 
-				GET https://www.chileatiende.cl/api/servicios/{servicioId}/fichas
+			GET https://www.chileatiende.cl/api/servicios/{servicioId}/fichas
 
-			Nota: El valor por defecto de type es "json", esto indica que los datos
-			devueltos por el metodo estan en ese formato. Si type = "xml", los datos
-			son devueltos como un documento XML.
+		Nota: El valor por defecto de type es "json", esto indica que los datos
+		devueltos por el metodo estan en ese formato. Si type = "xml", los datos
+		son devueltos como un documento XML.
 		"""
 		self._connection.request('GET','/api/servicios/'+servicioId+'/fichas?access_token='+self._access_token+'&type='+type+'&callback='+callback)
 		
@@ -131,25 +132,26 @@ class Fichas(ChileAtiende):
 
 class Servicios(ChileAtiende):
 	""" La clase Servicios representa a las instituciones que publican sus tramites (a traves de Fichas) 
-		en ChileAtiende. Deriva de las clase base ChileAtiende.
+	en ChileAtiende. Deriva de las clase base ChileAtiende.
 
-		ejemplo de uso:
-			from chileatiende.clientes import Servicios
+	Ejemplo de uso:
+		
+		from chileatiende.clientes import Servicios
 
-			servicios = Servicios(access_token=u'PiKjqM1XAx1P3Wia')
+		servicios = Servicios(access_token=u'PiKjqM1XAx1P3Wia')
 	"""
 	def __init__(self,access_token=None):
 		ChileAtiende.__init__(self,access_token)
 
 	def obtener(self,servicioId=None,type='json',callback=''):
 		""" Metodo que permite obtener los datos de un Servicio Publico, de acuerdo a su identificador 
-			unico servicioId. El metodo consume el servicio REST:
+		unico servicioId. El metodo consume el servicio REST:
 
-				GET https://www.chileatiende.cl/api/servicios/{servicioId}
+			GET https://www.chileatiende.cl/api/servicios/{servicioId}
 
-			Nota: El valor por defecto de type es "json", esto indica que los datos
-			devueltos por el metodo estan en ese formato. Si type = "xml", los datos
-			son devueltos como un documento XML.
+		Nota: El valor por defecto de type es "json", esto indica que los datos
+		devueltos por el metodo estan en ese formato. Si type = "xml", los datos
+		son devueltos como un documento XML.
 		"""
 		self._connection.request('GET','/api/servicios/'+servicioId+'?access_token='+self._access_token+'&type='+type+'&callback='+callback)
 		
@@ -162,13 +164,13 @@ class Servicios(ChileAtiende):
 
 	def listar(self,type='json',callback=''):
 		""" Metodo que permite listar todos los servicios registrados en ChileAtiende. 
-			El metodo consume el servicio REST:
+		El metodo consume el servicio REST:
 
-				GET https://www.chileatiende.cl/api/servicios
-				
-			Nota: El valor por defecto de type es "json", esto indica que los datos
-			devueltos por el metodo estan en ese formato. Si type = "xml", los datos
-			son devueltos como un documento XML.
+			GET https://www.chileatiende.cl/api/servicios
+
+		Nota: El valor por defecto de type es "json", esto indica que los datos
+		devueltos por el metodo estan en ese formato. Si type = "xml", los datos
+		son devueltos como un documento XML.
 		"""
 		self._connection.request('GET','/api/servicios?access_token='+self._access_token+'&type='+type+'&callback='+callback)
 		
